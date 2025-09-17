ea378b28f95d396b7bdc802852be87a5bb61fd14
Leandro Batista <leandrobouwierphp@gmail.com>
2025-09-17 16:45:11 -0300

    testes do streams  por baixo dos panos

--- src/server.js
+++ src/server.js
@@ -29,8 +29,21 @@ import http from "node:http";
 
 const users = [];
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async(req, res) => {
   const { method, url } = req;
+
+  const buffers = []
+
+  for await (const chunk of req) {
+      buffers.push(chunk)
+    }
+  
+  try{
+    req.body = JSON.parse(Buffer.concat(buffers).toString())
+  } catch{
+    req.body = null
+  }
+  
 
   if (method === "GET" && url === "/users") {
     return res
@@ -39,10 +52,12 @@ const server = http.createServer((req, res) => {
   }
 
   if (method === "POST" && url === "/users") {
+    const { name, email} = req.body
+
     users.push({
       id: 1,
-      name: "Leandro Batista",
-      email: "leandro@gmail.com",
+      name,
+      email
     });
     //
     return res.writeHead(201).end();
