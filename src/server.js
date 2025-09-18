07007aa87fa5c1ee49ff325a3a362c86acb70e01
Leandro Batista <leandrobouwierphp@gmail.com>
2025-09-17 23:06:10 -0300

    middleware

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import http from "node:http";
+import { json } from "./middleware/json.js";
 
 // - Criar usuários
 // - Listagem usuários
@@ -27,32 +28,23 @@ import http from "node:http";
 
 // Cabeçalhos (Requisição/resposta) => Metadados
 
+//middleware - nada mais é que um interceptador - um interceptador no node é uma função que ela vai interceptar nossa
+// requisição. São faceis de ser reconhecido porque sempre receberam "req" e "res"
+
 const users = [];
 
-const server = http.createServer(async(req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
-  const buffers = []
+  await json(req,res)
 
-  for await (const chunk of req) {
-      buffers.push(chunk)
-    }
-  
-  try{
-    req.body = JSON.parse(Buffer.concat(buffers).toString())
-  } catch{
-    req.body = null
-  }
-  
 
   if (method === "GET" && url === "/users") {
-    return res
-      .setHeader("Content-type", "application/json") // cabeçalho metadados
-      .end(JSON.stringify(users));
+    return res.end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
-    const { name, email} = req.body
+    const { name, email } = req.body
 
     users.push({
       id: 1,
