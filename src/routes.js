8dd2cc6f60c9686805cd75c331795f0844203c8a
Leandro Batista <leandrobouwierphp@gmail.com>
2025-09-21 12:14:47 -0300

    search e filter

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,35 @@
-import path from "node:path";
 import { randomUUID } from 'node:crypto'
 import { Database } from "./database.js";
-
-
+import { buildRoutePath } from './utils/build-route-path.js';
 const database = new Database()
+
+/*
+1) Query Parameters: URL Stateful => Usamos para Filtros, paginação, não obrigatorios 
+  localhost/3333/users?userId=1&name=Leandro - Usamos esse tipo pra enviar informações que não são sensiveis 
+
+2) Route Parameters: geralmente usado para identificação de recurso (exemplo usando para deletar). 
+  localhost/3333/users/1
+  
+3) Request Body:  Envio de informações de um formulario (HTTPS) ele não fica na URL
+*/
 
 export const routes = [
   {
     method: "GET",
-    path: "/users",
+    path: buildRoutePath('/users'),
     handler: (req, res) => {
-      const users = database.select("users");
+      const { search } = req.query
+      const users = database.select('users', search ? {
+        name: search,
+        email: search
+      } : null)
 
       return res.end(JSON.stringify(users));
     },
   },
   {
     method: "POST",
-    path: "/users",
+    path: buildRoutePath('/users'),
     handler: (req, res) => {
       const { name, email } = req.body;
 
@@ -27,8 +39,33 @@ export const routes = [
         email,
       };
       database.insert("users", user);
-
       return res.writeHead(201).end();
     },
   },
+  {
+    method: 'PUT',
+    path: buildRoutePath('/users/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+      const { name, email } = req.body
+
+      database.update('users', id, {
+        name,
+        email,
+      })
+
+      return res.writeHead(204).end()
+    }
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/users/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+
+      database.delete('users', id)
+
+      return res.writeHead(204).end()
+    }
+  }
 ];
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from "node:http";
 import { json } from "./middleware/json.js";
 import { routes } from "./routes.js";
+import { extractQueryParams } from './utils/extract-query-params.js'
 
 // - Criar usuários
 // - Listagem usuários
@@ -34,20 +35,25 @@ import { routes } from "./routes.js";
 
 //UUID => Unique univesal ID (Ela sempre vai retornar um ID unico)
 
-
-
-
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
   await json(req,res)
 
   const route = routes.find(route =>{
-    return route.method === method && route.path === url
+    return route.method === method && route.path.test(url)
   })
 
   if (route){
+    const routeParams = req.url.match(route.path)
+
+    const { query, ...params } = routeParams.groups
+
+    req.params = params
+    req.query = query ? extractQueryParams(query) : {}
+    
     return route.handler(req, res)
+
   }
 
   return res.writeHead(404).end();
